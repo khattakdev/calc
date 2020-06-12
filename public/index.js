2968d73c4219addadc57ed7348f19e4e31ae7ceb
Khattak <akkhattak65@gmail.com>
2020-06-12 23:37:00 +0500

    :sparkles: Add Functionalities

---
+++ public/index.js
@@ -0,0 +1,126 @@
+import * as selector from "./selectors.js";
+
+// Result Bar
+const resultBar = document.querySelector(".result");
+const logsBar = document.querySelector(".logs");
+let result = 0,
+  record = [],
+  aOps = [],
+  equaled = false,
+  isSign = false;
+
+// Numbers Event Listeners
+for (const key in selector.btns) {
+  selector.btns[key].addEventListener("click", buttonHandler);
+}
+
+selector.clearBtn.addEventListener("click", () => {
+  logsBar.innerHTML = "";
+  resultBar.innerHTML = "";
+  record = [];
+  aOps = [];
+  isSign = false;
+  result = 0;
+});
+
+selector.backspaceBtn.addEventListener("click", () => {
+  if (!resultBar.innerText) return;
+  let tempArr = resultBar.innerText.split("");
+  tempArr.pop();
+  resultBar.innerText = tempArr.join("");
+});
+
+// Arithmatic Operations
+for (const key in selector.operations) {
+  selector.operations[key].addEventListener("click", function (e) {
+    // Return If NO number is entered or just Dot is entered
+    if (!isSign && (!result || result == ".")) return;
+
+    // If Previous Button was Arithmatic Operation
+    if (isSign) {
+      aOps[aOps.length - 1] = e.target.innerHTML;
+      let tempArr = logsBar.innerText.split("");
+      tempArr[tempArr.length - 1] = e.target.innerHTML;
+      logsBar.innerText = tempArr.join("");
+      return;
+    }
+
+    // If Previous Button was a number
+    record.push(result);
+    aOps.push(e.target.innerHTML);
+    logsBar.innerText += e.target.innerText;
+    result = 0;
+    isSign = true;
+  });
+}
+
+selector.equalBtn.addEventListener("click", () => {
+  record.push(result);
+  result = 0;
+  console.log(record, aOps);
+
+  for (let i = 0; i < record.length; i++) {
+    switch (aOps[i]) {
+      case "+": {
+        record[i + 1] = Number(record[i]) + Number(record[i + 1]);
+        break;
+      }
+      case "-": {
+        record[i + 1] = Number(record[i]) - Number(record[i + 1]);
+        break;
+      }
+      case "/": {
+        record[i + 1] = Number(record[i]) / Number(record[i + 1]);
+        break;
+      }
+      case "*": {
+        record[i + 1] = Number(record[i]) * Number(record[i + 1]);
+        break;
+      }
+      case "%": {
+        record[i + 1] = Number(record[i]) % Number(record[i + 1]);
+        break;
+      }
+    }
+
+    var temp = record[record.length - 1];
+    record = [temp];
+    equaled = true;
+    console.log(record);
+    resultBar.innerText = temp;
+  }
+});
+
+// Sign Change BTN
+selector.plusMinusBtn.addEventListener("click", () => {
+  var num = Number(resultBar.innerText);
+
+  if (Math.sign(num) == 1 || Math.sign(num) == -1) {
+    num = num * -1;
+    resultBar.innerText = num;
+  }
+});
+function buttonHandler(e) {
+  // If User Pressed Equal Last time
+  if (equaled) {
+    record = [];
+    isSign = false;
+    result = 0;
+    aOps = [];
+    equaled = false;
+    logsBar.innerText = "";
+  }
+  // Special Case for DOT (.)
+  if (e.target.innerHTML == ".") {
+    if (logsBar.innerText.includes(".")) return;
+  }
+  if (logsBar.innerHTML == "&nbsp;") {
+    result = e.target.innerText;
+    isSign = false;
+    logsBar.innerText = e.target.innerText;
+    return;
+  }
+  result += e.target.innerText;
+  isSign = false;
+  logsBar.innerText += e.target.innerText;
+}
---
+++ public/selectors.js
@@ -0,0 +1,48 @@
+// Buttons
+export const backspaceBtn = document.querySelector(".buttons-c");
+export const clearBtn = document.querySelector(".buttons-cc");
+export const plusMinusBtn = document.querySelector(".buttons-pm");
+// export const divideBtn = document.querySelector(".buttons-divide");
+// export const multiplyBtn = document.querySelector(".buttons-multiply");
+// export const minusBtn = document.querySelector(".buttons-minus");
+// export const plusBtn = document.querySelector(".buttons-plus");
+// export const modBtn = document.querySelector(".buttons-mod");
+export const equalBtn = document.querySelector(".buttons-equal");
+// export const dotBtn = document.querySelector(".buttons-dot");
+// Numbers
+// export const oneBtn = document.querySelector(".buttons-one");
+// export const twoBtn = document.querySelector(".buttons-two");
+// export const threeBtn = document.querySelector(".buttons-three");
+// export const fourBtn = document.querySelector(".buttons-four");
+// export const fiveBtn = document.querySelector(".buttons-five");
+// export const sixBtn = document.querySelector(".buttons-six");
+// export const sevenBtn = document.querySelector(".buttons-seven");
+// export const eightBtn = document.querySelector(".buttons-eight");
+// export const nineBtn = document.querySelector(".buttons-nine");
+// export const zeroBtn = document.querySelector(".buttons-zero");
+
+export const btns = {
+  //   backspaceBtn: document.querySelector(".buttons-c"),
+  //   clearBtn: document.querySelector(".buttons-cc"),
+  //   equalBtn: document.querySelector(".buttons-equal"),
+  dotBtn: document.querySelector(".buttons-dot"),
+  oneBtn: document.querySelector(".buttons-one"),
+  twoBtn: document.querySelector(".buttons-two"),
+  threeBtn: document.querySelector(".buttons-three"),
+  fourBtn: document.querySelector(".buttons-four"),
+  fiveBtn: document.querySelector(".buttons-five"),
+  sixBtn: document.querySelector(".buttons-six"),
+  sevenBtn: document.querySelector(".buttons-seven"),
+  eightBtn: document.querySelector(".buttons-eight"),
+  nineBtn: document.querySelector(".buttons-nine"),
+  zeroBtn: document.querySelector(".buttons-zero"),
+};
+
+export const operations = {
+  //   plusMinusBtn: document.querySelector(".buttons-pm"),
+  divideBtn: document.querySelector(".buttons-divide"),
+  multiplyBtn: document.querySelector(".buttons-multiply"),
+  minusBtn: document.querySelector(".buttons-minus"),
+  plusBtn: document.querySelector(".buttons-plus"),
+  modBtn: document.querySelector(".buttons-mod"),
+};
